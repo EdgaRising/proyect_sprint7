0228fd7ccea8bba1b9e977e40ce2515e5dd3defc
EdgaRising <e.omar.rc@gmail.com>
2025-06-09 00:02:53 -0600

    Paso 4 concluido

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # proyect_sprint7
+# Aplicacion basada en Streamlit, construye un histograma y un un gráfico de dispersión
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv("../vehicles_us.csv")
+car_data = pd.read_csv("vehicles_us.csv")
 st.header('Mi primer proyecto web')
 # st.write('Esta aplicación aún no es funcional. En construcción.')
 hist_button = st.button('Construir histograma')  # crear un botón
